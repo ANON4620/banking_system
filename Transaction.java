095c0118f226b912d5c3c471ab6c91b71beec833
ANON4620 <swarnadeep.click@gmail.com>
2026-02-27 00:35:51 +0530

    feat: add date and time members

--- Transaction.java
+++ Transaction.java
@@ -1,16 +1,23 @@
 package banking_system;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Transaction {
     private long accountNumber;
     private String name;
     private double amount;
     private String type;
+    private String dateAndTime;
 
     Transaction(long accountNumber, String name, double amount, String type) {
         this.accountNumber = accountNumber;
         this.name = name;
         this.amount = amount;
         this.type = type;
+        LocalDateTime dateAndTime = LocalDateTime.now();
+        DateTimeFormatter dtFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        this.dateAndTime = dateAndTime.format(dtFormat);
     }
 
     long getAccountNumber() {
@@ -27,6 +34,10 @@ public class Transaction {
 
     String getType() {
         return type;
+    }
+
+    String getDateAndTime() {
+        return dateAndTime;
     }
 
 }
